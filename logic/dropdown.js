c3fac4130305c4897b0724bffdce0a630236b529
SPHS Key Club <167732586+sphskeyclub@users.noreply.github.com>
2024-04-23 20:14:28 -0400

    Added mobile dropdown

---
+++ logic/dropdown.js
@@ -0,0 +1,9 @@
+function getDropdown() {
+    document.getElementById("mainWindow").style.display = 'none';
+    document.getElementById("mobileDropdown").style.display = 'block';
+}
+
+function closeDropdown() {
+    document.getElementById("mainWindow").style.display = 'block';
+    document.getElementById("mobileDropdown").style.display = 'none';
+}
