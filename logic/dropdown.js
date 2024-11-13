1459501b27f9b97f3af3040968ee8b78016f05dd
SPHS Key Club <167732586+sphskeyclub@users.noreply.github.com>
2024-11-13 13:48:57 -0500

    Add files via upload

--- logic/dropdown.js
+++ logic/dropdown.js
@@ -7,3 +7,13 @@ function closeDropdown() {
     document.getElementById("mainWindow").style.display = 'block';
     document.getElementById("mobileDropdown").style.display = 'none';
 }
+
+function getDropdownAbout() {
+    document.getElementById("mainWindow").style.display = 'none';
+    document.getElementById("aboutDropdown").style.display = 'block';
+}
+
+function closeDropdownAbout() {
+    document.getElementById("mainWindow").style.display = 'block';
+    document.getElementById("aboutDropdown").style.display = 'none';
+}
