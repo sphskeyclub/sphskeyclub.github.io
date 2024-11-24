63d534ab3aa111d53738c4fe035454ed7dba0e9f
SPHS Key Club <167732586+sphskeyclub@users.noreply.github.com>
2024-11-24 00:31:08 -0500

    Fix dropdown

--- logic/dropdown.js
+++ logic/dropdown.js
@@ -1,19 +1,37 @@
+var onMobile = false;
+
 function getDropdown() {
+    document.getElementById("home").style.display = 'none';
     document.getElementById("mainWindow").style.display = 'none';
     document.getElementById("mobileDropdown").style.display = 'block';
+
+    onMobile = true;
 }
 
 function closeDropdown() {
     document.getElementById("mainWindow").style.display = 'block';
+    document.getElementById("home").style.display = 'block';
     document.getElementById("mobileDropdown").style.display = 'none';
+
+    onMobile = false;
 }
 
 function getDropdownAbout() {
-    document.getElementById("mainWindow").style.display = 'none';
+    if (onMobile) {
+        document.getElementById("mobileDropdown").style.display = 'none';
+    } else {
+        document.getElementById("home").style.display = 'none';
+        document.getElementById("mainWindow").style.display = 'none';
+    }
     document.getElementById("aboutDropdown").style.display = 'block';
 }
 
 function closeDropdownAbout() {
-    document.getElementById("mainWindow").style.display = 'block';
+    if (onMobile) {
+        document.getElementById("mobileDropdown").style.display = 'block';
+    } else {
+        document.getElementById("home").style.display = 'block';
+        document.getElementById("mainWindow").style.display = 'block';
+    }
     document.getElementById("aboutDropdown").style.display = 'none';
 }
