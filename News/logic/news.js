15723c4ef2d21a313cc7cc1d29c6317b1a3250fa
boilerplate <jmuet531@outlook.com>
2025-05-03 09:08:55 -0400

    added 4/29 newsletter (praying this works)

--- News/logic/news.js
+++ News/logic/news.js
@@ -1,20 +1,20 @@
 let data = {
     "articles": [
-        // {
-        //     "title": "April 29th Newsletter",
-        //     "classification": "NEWSLETTER",
-        //     "class-style": "margin: 0; color: var(--KeyBlue)",
-        //     "author": "Aurora Henderson",
-        //     "position": "Editor",
-        //     "year": "2025",
-        //     "bio": "Our members who attended DCON picked up tons of memories (and awards, too!) over the weekend. Let's give them a warm Key-style welcome back with continued service throughout the final month of school!",
-        //     "url-bio": "Read this article by ",
-        //     "url": "",
-        //     "img": {
-        //         "srcset": "./articles/imgs/PointOpp/KeyPoints-500w.webp 500w, ./articles/imgs/PointOpp/KeyPoints-1000w.webp 1000w, ./articles/imgs/PointOpp/KeyPoints-1280w.webp 1280w",
-        //         "alt": "SPHS Key Club Banner"
-        //     }
-        // },
+        {
+            "title": "April 29th Newsletter",
+            "classification": "NEWSLETTER",
+            "class-style": "margin: 0; color: var(--KeyBlue)",
+            "author": "Aurora Henderson",
+            "position": "Editor",
+            "year": "2025",
+            "bio": "Our members who attended DCON picked up tons of memories (and awards, too!) over the weekend. Let's give them a warm Key-style welcome back with continued service throughout the final month of school!",
+            "url-bio": "Read this article by ",
+            "url": "https://sphskeyclub.org/News/newsletters/2025/April29thNewsletter.pdf",
+            "img": {
+                "srcset": "./articles/imgs/PointOpp/KeyPoints-500w.webp 500w, ./articles/imgs/PointOpp/KeyPoints-1000w.webp 1000w, ./articles/imgs/PointOpp/KeyPoints-1280w.webp 1280w",
+                "alt": "SPHS Key Club Banner"
+            }
+        },
         {
             "title": "April 22nd Newsletter",
             "classification": "NEWSLETTER",
