3a9d714d11d600ce6bdaa7551a80144b67b6672a
boilerplate <jmuet531@outlook.com>
2025-08-27 15:36:07 -0400

    forgot 1000w; also I typed 3 ! s in the last commit message and it converted to two exclamation marks (last terminal command) + ! :P

--- imgs/make_img.sh
+++ imgs/make_img.sh
@@ -12,7 +12,7 @@ fi
 
 # Convert the file to multiple WebP sizes
 echo "Converting $filename to multiple WebP sizes..."
-#add 3526 and 450 for home image
+#add 3526, 100, and 450 for home image
 for size in 300 721 1080 1420 1690 1920 2048 2500; do
   magick "$filename" -resize ${size}x -quality 80 "${new_name}-${size}w.webp"
 done
